05328b14ce6947d78cafbbda38d6fac584c0da75
Student Aman4580 <amanverma7124@gmail.com>
2024-07-27 23:57:46 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Home from "./routes/Home";
 import Shop from "./routes/Shop"
 import Schemes from "./routes/Schemes";
 import Helpdesk from "./routes/Helpdesk";
-// import Predict from "./routes/Predict";
+import Predict from "./routes/Predict";
 // import PredictCrop from "./routes/PredictCrop";
 // import Homepage from "./routes/Homepage";
 // import CartReview from "./routes/CartReview";
@@ -24,8 +24,8 @@ function App() {
           <Route exact path="/shop" element={<Shop />} />
           <Route exact path="/schemes" element={<Schemes />} />
           <Route exact path="/helpdesk" element={<Helpdesk />} />
-          {/* <Route exact path="/predict" element={<Predict />} />
-          <Route exact path="/predict/crop" element={<PredictCrop />} />
+          <Route exact path="/predict" element={<Predict />} />
+          {/* <Route exact path="/predict/crop" element={<PredictCrop />} />
           <Route exact path="/predict/loan" element={<Predictloan />} />
           <Route exact path="/marketplace" element={<Homepage/>} />
           <Route exact path="/review/cart" element={<CartReview/>} />
