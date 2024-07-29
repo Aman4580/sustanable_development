82876eb37221f77920b962d6db947389b55ded75
Student Aman4580 <amanverma7124@gmail.com>
2024-07-29 23:34:25 +0530

    commit

--- src/components/buy/CartReviewItem/CartReviewItem.jsx
+++ src/components/buy/CartReviewItem/CartReviewItem.jsx
@@ -1,26 +1,27 @@
 import React from "react";
-import { Button, Card } from "react-bootstrap";
+import PropTypes from "prop-types";
 import "./CartReviewItem.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCoffee,
-  faShoppingCart,
-  faTimes,
-} from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+
 const CartReviewItem = (props) => {
   const { title, quantity, keys, img } = props.foodsToReview;
   return (
     <div className="container text-center mt-3">
       <div className="row">
         <div className="col-md-4 crd">
-          <img src={img} alt="" />
+          <img src={img} alt={title} />
         </div>
         <div className="col-md-8">
           <h5>
-            {title}&nbsp;&nbsp; <br /> Quantity : {quantity}{" "}
-            <a onClick={() => props.removeItem(keys)} href="#">
+            {title}&nbsp;&nbsp; <br /> Quantity: {quantity}{" "}
+            <button
+              onClick={() => props.removeItem(keys)}
+              className="btn btn-link p-0"
+              aria-label="Remove item"
+            >
               <FontAwesomeIcon icon={faTimes} />
-            </a>
+            </button>
           </h5>
         </div>
       </div>
@@ -28,8 +29,15 @@ const CartReviewItem = (props) => {
     </div>
   );
 };
-{
-  /* <Button size="sm" onClick={() => props.removeItem(keys)} variant="danger">Remove Item</Button> */
-}
+
+CartReviewItem.propTypes = {
+  foodsToReview: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    quantity: PropTypes.number.isRequired,
+    keys: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+  }).isRequired,
+  removeItem: PropTypes.func.isRequired,
+};
 
 export default CartReviewItem;
