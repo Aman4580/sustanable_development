9e4d4fdec920e766f37f52c8c0147681fe0ce436
Student Aman4580 <amanverma7124@gmail.com>
2024-07-30 23:02:49 +0530

    commit

--- src/components/buy/FoodCategory/FoodCategory.jsx
+++ src/components/buy/FoodCategory/FoodCategory.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Card } from "react-bootstrap";
 import "./FoodCategory.css";
 import { Link } from "react-router-dom";
-
+// food category function
 const FoodCategory = (props) => {
   const { keys,title, subtitle, img, catagories, price } = props.items;
   return (
